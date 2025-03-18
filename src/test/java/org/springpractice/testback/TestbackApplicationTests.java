2e9ca33e375b096467940635a83145dfaeac62c9
Xen-alpha <hky7548@naver.com>
2025-03-18 11:30:40 +0900

    [Init] first commit

---
+++ src/test/java/org/springpractice/testback/TestbackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springpractice.testback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
